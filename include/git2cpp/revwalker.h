61c5c221c919b05729f65c6c81d6317270eeb51f
Andrey Davydov <andrey.a.davydov@gmail.com>
2016-01-09 19:47:08 +0300

    code format

--- include/git2cpp/revwalker.h
+++ include/git2cpp/revwalker.h
@@ -6,52 +6,52 @@
 
 namespace git
 {
-   struct Repository;
+    struct Repository;
 
-   struct RevWalker
-   {
-      RevWalker(git_revwalk * walker, Repository const & repo)
-         : walker_(walker)
-         , repo_(repo)
-      {}
+    struct RevWalker
+    {
+        RevWalker(git_revwalk * walker, Repository const & repo)
+            : walker_(walker)
+            , repo_(repo)
+        {}
 
-      ~RevWalker();
+        ~RevWalker();
 
-      enum class sorting : unsigned int
-      {
-         none        = GIT_SORT_NONE,
-         topological = GIT_SORT_TOPOLOGICAL,
-         time        = GIT_SORT_TIME,
-         reverse     = GIT_SORT_REVERSE
-      };
+        enum class sorting : unsigned int
+        {
+            none        = GIT_SORT_NONE,
+            topological = GIT_SORT_TOPOLOGICAL,
+            time        = GIT_SORT_TIME,
+            reverse     = GIT_SORT_REVERSE
+        };
 
-      friend sorting operator ~ (sorting);
-      friend sorting operator | (sorting, sorting);
-      friend sorting operator & (sorting, sorting);
-      friend sorting operator ^ (sorting, sorting);
+        friend sorting operator ~ (sorting);
+        friend sorting operator | (sorting, sorting);
+        friend sorting operator & (sorting, sorting);
+        friend sorting operator ^ (sorting, sorting);
 
-      void sort(sorting);
-      void simplify_first_parent();
+        void sort(sorting);
+        void simplify_first_parent();
 
-      void push_head() const;
-      void hide(git_oid const &) const;
-      void push(git_oid const &) const;
+        void push_head() const;
+        void hide(git_oid const &) const;
+        void push(git_oid const &) const;
 
-      Commit  next()                 const;
-      bool    next(char * id_buffer) const;
+        Commit  next()                 const;
+        bool    next(char * id_buffer) const;
 
-      RevWalker               (RevWalker const &) = delete;
-      RevWalker& operator =   (RevWalker const &) = delete;
+        RevWalker               (RevWalker const &) = delete;
+        RevWalker& operator =   (RevWalker const &) = delete;
 
-      RevWalker(RevWalker && other)
-         : walker_(other.walker_)
-         , repo_(other.repo_)
-      {
-         other.walker_ = nullptr;
-      }
+        RevWalker(RevWalker && other)
+            : walker_(other.walker_)
+            , repo_(other.repo_)
+        {
+            other.walker_ = nullptr;
+        }
 
-   private:
-      git_revwalk * walker_;
-      Repository const & repo_;
-   };
+    private:
+        git_revwalk * walker_;
+        Repository const & repo_;
+    };
 }
