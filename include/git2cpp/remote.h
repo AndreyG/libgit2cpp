d290f4436b4067387343bdc00f20a069103103d2
Andrey Davydov <andrey.davydov@jetbrains.com>
2020-05-01 20:15:52 +0300

    initial implementation of Remote::fetch, only few options are supported

--- include/git2cpp/remote.h
+++ include/git2cpp/remote.h
@@ -3,6 +3,9 @@
 #include <memory>
 
 struct git_remote;
+struct git_oid;
+struct git_transfer_progress;
+struct git_cred;
 
 namespace git
 {
@@ -10,6 +13,17 @@ namespace git
     {
         const char * url()      const;
         const char * pushurl()  const;
+
+        struct FetchCallbacks
+        {
+            virtual void update_tips(char const * refname, git_oid const & a, git_oid const & b) {}
+            virtual void sideband_progress(char const * str, int len) {}
+            virtual void transfer_progress(git_transfer_progress const &) {}
+
+            virtual git_cred* acquire_cred(const char * url, const char * username_from_url, unsigned int allowed_types) = 0;
+        };
+
+        void fetch(FetchCallbacks &, char const * reflog_message = nullptr);
 
     private:
         friend struct Repository;
--- src/remote.cpp
+++ src/remote.cpp
@@ -14,6 +14,41 @@ namespace git
         return git_remote_pushurl(remote_.get());
     }
 
+    void Remote::fetch(FetchCallbacks & callbacks, char const * reflog_message)
+    {
+        git_fetch_options opts = GIT_FETCH_OPTIONS_INIT;
+        opts.callbacks.payload = &callbacks;
+
+        opts.callbacks.update_tips = [] (char const * refname, git_oid const * a, git_oid const * b, void * data)
+        {
+            auto callbacks = static_cast<FetchCallbacks*>(data);
+            callbacks->update_tips(refname, *a, *b);
+            return 0;
+        };
+        opts.callbacks.sideband_progress = [] (char const * str, int len, void * data)
+        {
+            auto callbacks = static_cast<FetchCallbacks*>(data);
+            callbacks->sideband_progress(str, len);
+            return 0;
+        };
+        opts.callbacks.transfer_progress = [] (git_transfer_progress const * stats, void * data)
+        {
+            auto callbacks = static_cast<FetchCallbacks*>(data);
+            callbacks->transfer_progress(*stats);
+            return 0;
+        };
+        opts.callbacks.credentials = [] (git_cred ** out, char const *url, char const * user_from_url, unsigned int allowed_types, void * data)
+        {
+            auto callbacks = static_cast<FetchCallbacks*>(data);
+            auto cred = callbacks->acquire_cred(url, user_from_url, allowed_types);
+            if (!cred)
+                return -1;
+            *out = cred;
+            return 0;
+        };
+        git_remote_fetch(remote_.get(), nullptr, &opts, reflog_message);
+    }
+
     void Remote::Destroy::operator()(git_remote * remote) const
     {
         git_remote_free(remote);
