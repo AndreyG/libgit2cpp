e6c2545c8696c274b3d17ec5df9be6349f3adb99
GravisZro <noreply+github@adaptivetime.com>
2023-06-04 07:29:43 -0400

    Update code for use with libgit2 version 1.7.0

--- src/id_to_str.cpp
+++ src/id_to_str.cpp
@@ -6,12 +6,12 @@ namespace git
 {
     std::string id_to_str(git_oid const & oid)
     {
-        return id_to_str(oid, GIT_OID_HEXSZ);
+        return id_to_str(oid, GIT_OID_SHA1_HEXSIZE);
     }
 
     std::string id_to_str(git_oid const & oid, size_t digits_num)
     {
-        char buf[GIT_OID_HEXSZ + 1];
+        char buf[GIT_OID_SHA1_HEXSIZE + 1];
         git_oid_tostr(buf, sizeof(buf), &oid);
         return std::string(buf, buf + digits_num);
     }
